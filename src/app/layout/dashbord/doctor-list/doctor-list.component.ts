5aec539d594c08b878f4e1a791e1bd45c127226f
Abdelrahaman-Nader <abualiya.2202@gmail.com>
2023-05-07 08:15:17 +0300

    issu about rebet doctor list

--- src/app/layout/dashbord/doctor-list/doctor-list.component.ts
+++ src/app/layout/dashbord/doctor-list/doctor-list.component.ts
@@ -3,18 +3,39 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-doctor-list',
   templateUrl: './doctor-list.component.html',
-  styleUrls: ['./doctor-list.component.scss']
+  styleUrls: ['./doctor-list.component.scss'],
 })
 export class DoctorListComponent implements OnInit {
+  constructor() {}
+  icon: string =
+    '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />';
 
-  constructor() { }
-
-@Input() datas: any = [
-    { icons: 'doctors.png'},
-  ]
-
+  @Input() data: any = [
+    {
+      photo: 'photo (2).png',
+      name: 'islam',
+      nots: 'Doctors joined this week',
+      matIcon: 'icon',
+    },
+    {
+      photo: 'photo (3).png',
+      name: 'Ahmed',
+      nots: 'Doctors joined this week',
+      matIcon: 'icon',
+    },
+    {
+      photo: 'photo (4).png',
+      name: 'Abdo',
+      nots: 'Doctors joined this week',
+      matIcon: 'icon',
+    },
+    {
+      photo: 'photo (5).png',
+      name: 'Gasser',
+      nots: 'Doctors joined this week',
+      matIcon: 'icon',
+    },
+  ];
 
   ngOnInit(): void {}
-
-
 }
